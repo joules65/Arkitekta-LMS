c83cf9fe824f5ef1d81a2aea40ff5ef0990ccb59
Ebuka <davidachibiri8@gmail.com>
2025-06-02 13:50:40 +0200

    Section Rendering Issue resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
-import {Section} from "lucide-react";
+
 import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionList";
 
@@ -9,14 +9,14 @@ const Page = () => {
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
         {/* Placeholder Data removed to be used later for Onboarding functionality. */}
-        <Section className="home-section">
+        <section className="home-section">
             <CompanionCard />
             <CompanionCard/>
             <CompanionCard/>
-        </Section>
-        <Section className="home-section">
+        </section>
+        <section className="home-section">
             <CompanionList />
-        </Section>
+        </section>
     </main>
   )
 }
